d3a026e41a8fbe5f3a62d5cd4d5f9b8739ac36cd
juliazib <julia.zibordi@gmail.com>
2022-05-16 14:37:22 -0300

    Codificação para base 64 funcionando.

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ let radioButton = document.getElementById("radioBtn")
 let codificar = document.getElementById("codificar")
 let decodificar = document.getElementById("decodificar")
 let codificarDecodificar = document.getElementById("submit")
+let entradaCifra = document.querySelector("#metodo")
+let mensagemDevolvida = document.getElementById("mensagemDevolvida")
 
 // Esconde ou mostra opção de deslocamento de acordo com a escolha do usuário
 function selecao() {
-    let entradaCifra = document.querySelector("#metodo")
     let deslocamentoCifra = document.getElementById("deslocamento")
     entradaCifra.addEventListener("click", function() {
         if (entradaCifra.value === "cifraCesar") {
@@ -27,6 +28,39 @@ radioButton.addEventListener("click", function() {
         codificarDecodificar.innerText = "Decodificar"
     }
     
+})
+
+
+// Codifica e decodifica mensagems em base64
+function base64() {
+    let mensagem = document.getElementById("mensagemRecebida").value
+    if (codificar.checked) {
+        let encodeBase64 = btoa(mensagem)
+        return encodeBase64
+    } 
+    else if (decodificar.checked) {
+        let decodeBase64 = atob(mensagem)
+        return decodeBase64
+    }
+    
+}
+
+function cifraDeCesar() {
+    let mensagem = document.getElementById("mensagemAntes").value
+    let incremento = document.getElementById("incremento").value
+    let mensagemCodificada = ""
+    let mensagemDecodificada = ""
+}
+
+codificarDecodificar.addEventListener("click", function(event) {
+    event.preventDefault()
+    if (entradaCifra.value === "cifraCesar") {
+        mensagemDevolvida.innerHTML = cifraDeCesar()
+        
+    }
+    else if (entradaCifra.value === "base64") {
+        mensagemDevolvida.innerHTML = base64()
+    }
 })
 
 
